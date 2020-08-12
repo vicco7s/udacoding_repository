b686392b3da836074d7efc7018b03503725caa53
gadang_sewu <viccodwipramadian@gmail.com>
2020-08-12 20:19:23 +0800

    Bmi_calculator Commit

---
+++ bmi_calculator/lib/page/input.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InputBmi extends StatefulWidget {
+  InputBmi({Key key}) : super(key: key);
+
+  @override
+  _InputBmiState createState() => _InputBmiState();
+}
+
+class _InputBmiState extends State<InputBmi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
