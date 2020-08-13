5cd1136039df21bb25b9fae5eaaf1273f062de09
gadang_sewu <viccodwipramadian@gmail.com>
2020-08-13 14:23:23 +0800

    BMI commit

--- bmi_calculator/lib/page/input.dart
+++ bmi_calculator/lib/page/input.dart
@@ -3,6 +3,7 @@ import 'package:bmi_calculator/components/gender.dart';
 import 'package:bmi_calculator/components/iconbutton.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 
 enum Gender{
@@ -19,131 +20,171 @@ class InputBmi extends StatefulWidget {
 class _InputBmiState extends State<InputBmi> {
   Gender selectedGender; 
   int age = 20;
+  int height = 150;
+  int weight = 60;
 
-
+// ignore: unused_element
+void _showDialog() {
+   showDialog(
+     context: context,
+     builder: (BuildContext context) {
+       return NumberPickerDialog.integer(
+         minValue: 10, 
+         maxValue: 250, 
+         initialIntegerValue: height);
+     }
+   ).then((value) {
+     if (value != null) {
+       setState(() {
+         height = value;
+       });
+     }
+   });
+ }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-         children: [
-           Row(
-             children: [
-               Padding(
-                 padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-                 child: Text('BMI Calculator',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-               ),
-             ],
-           ),
-           Row(
-             children: [
-               Padding(
-                 padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                 child: Text('Gender kamu ?',),
-               )
-             ],
-           ),
-          Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                   child: Result_Gender(
-                     onPress: (){
-                       setState(() {
-                         selectedGender = Gender.male;
-                       });
-                     },
-                     colour: selectedGender == Gender.male ? kActiveColor : kDisableColor,
-                     chardchild: Icon(FontAwesomeIcons.mars)
-                     ), 
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text('Cowok'),
-                  )
-                ],
-              ),
-              SizedBox(width: 50,),
-               Column(
-                 children: [
-                   Container(
-                   child: Result_Gender(
-                     onPress: (){
-                       setState(() {
-                         selectedGender = Gender.famale;
-                       });
-                     },
-                     colour: selectedGender == Gender.famale ? kActiveColor : kDisableColor,
-                     chardchild: Icon(FontAwesomeIcons.venus),), 
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Text('Cewek'),
-                    )
-                 ],
-               )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 30),
-            child: Divider(
-             color: Colors.white, 
-            ),
-          ),
-
-            Row(
+      body: SafeArea(
+          child: Container(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+           children: [
+             Row(
                children: [
-                 Text('Umur kamu berapa ?'),
+                 Padding(
+                   padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                   child: Text('BMI Calculator',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                 ),
+               ],
+             ),
+             Row(
+               children: [
+                 Padding(
+                   padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                   child: Text('Gender kamu ?',),
+                 )
                ],
              ),
             Row(
-             children: [
-               Container(
-                padding: EdgeInsets.fromLTRB(0, 20, 10, 20),
-                child: Text(
-                 age.toString(),
-                 style: TextStyle(fontSize: 60,color: Colors.white), 
-                ), 
-               ),
-               Column(
-                children: [
-                  ButtonIcon(
-                    onPress: (){
-                      setState(() {
-                        age++;
-                      });
-                    },
-                    icon: Icons.arrow_drop_up,
-                  ),
-                  SizedBox(height: 10,),
-                  ButtonIcon(
-                    onPress: (){
-                      setState(() {
-                        age--;
-                      });
-                    },
-                    icon: Icons.arrow_drop_down,
-                  )
-                ], 
-               ),
-             ], 
+              children: [
+                Column(
+                  children: [
+                    Container(
+                     child: Result_Gender(
+                       onPress: (){
+                         setState(() {
+                           selectedGender = Gender.male;
+                         });
+                       },
+                       colour: selectedGender == Gender.male ? kActiveColor : kDisableColor,
+                       chardchild: Icon(FontAwesomeIcons.mars)
+                       ), 
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text('Cowok'),
+                    )
+                  ],
+                ),
+                SizedBox(width: 50,),
+                 Column(
+                   children: [
+                     Container(
+                     child: Result_Gender(
+                       onPress: (){
+                         setState(() {
+                           selectedGender = Gender.famale;
+                         });
+                       },
+                       colour: selectedGender == Gender.famale ? kActiveColor : kDisableColor,
+                       chardchild: Icon(FontAwesomeIcons.venus),), 
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text('Cewek'),
+                      )
+                   ],
+                 )
+              ],
             ),
             Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-            child: Divider(
-             color: Colors.white, 
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 30),
+              child: Divider(
+               color: Colors.white, 
+              ),
             ),
-          ),
 
-  
-          
-         ], 
-        ), 
-       ), 
+              Row(
+                 children: [
+                   Text('Umur kamu berapa ?'),
+                 ],
+               ),
+              Row(
+               children: [
+                 Container(
+                  padding: EdgeInsets.fromLTRB(0, 20, 10, 20),
+                  child: Text(
+                   age.toString(),
+                   style: TextStyle(fontSize: 60,color: Colors.white), 
+                  ), 
+                 ),
+                 Column(
+                  children: [
+                    ButtonIcon(
+                      onPress: (){
+                        setState(() {
+                          age++;
+                        });
+                      },
+                      icon: Icons.arrow_drop_up,
+                    ),
+                    SizedBox(height: 10,),
+                    ButtonIcon(
+                      onPress: (){
+                        setState(() {
+                          age--;
+                        });
+                      },
+                      icon: Icons.arrow_drop_down,
+                    )
+                  ], 
+                 ),
+               ], 
+              ),
+              Padding(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+              child: Divider(
+               color: Colors.white, 
+              ),
+            ),
+
+            Column(
+             children: [
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                   Text('Tinggi badan mu ?'),
+                   Text('Berat badan mu ?')
+                 ],
+               ),
+               SizedBox(height: 10,),
+               ListTile(
+                leading: InkWell(
+                      onTap: (){
+                       _showDialog();
+                      },
+                      child: Text(height.toString(),style: TextStyle(fontSize: 50,))),
+                title: Text('cm'),
+               )
+             ], 
+            )
+            
+           ], 
+          ), 
+         ),
+      ), 
     );
   }
 }
