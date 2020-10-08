2a2c1458acda3aace658975e9f1a07d7a67cf81c
Vicco Dwi <50991950+vicco7s@users.noreply.github.com>
2020-10-08 20:35:42 +0800

    comiit 2

--- mahasiswa_app/lib/page/HomeScreen.dart
+++ mahasiswa_app/lib/page/HomeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mahasiswa_app/constant/cons.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,7 +11,19 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text('Mahasiswa',style: alerTextT,),
+      ),
+      body: ListView.builder(
+        itemCount: 2,
+        itemBuilder: (context,i) {
+          return Container(
+            child: Text('testing'),
+          );
+        }
+      ),
     );    
   }
 }
