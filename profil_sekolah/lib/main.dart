619327d72881826e3ba8654d6bf5e835c60989c3
Begal <50991950+vicco7s@users.noreply.github.com>
2020-12-08 20:55:53 +0800

    Update Main.dart
    update main .dart add firebase inisiasi

--- profil_sekolah/lib/main.dart
+++ profil_sekolah/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:profil_sekolah/screen/splash.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
