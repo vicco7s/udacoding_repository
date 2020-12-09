7efff28fa4fb5670e98863e9eea15f27cf2b10bd
Begal <50991950+vicco7s@users.noreply.github.com>
2020-12-09 15:40:59 +0800

    Update detail.dart

--- profil_sekolah/lib/page/detail.dart
+++ profil_sekolah/lib/page/detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:profil_sekolah/models/modelFirebase.dart';
+import 'package:profil_sekolah/util/const.dart';
 
 class DetailPage extends StatefulWidget {
   final SchoolModel school;
@@ -12,8 +13,31 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        elevation: 0.0,
+        centerTitle: true,
+        backgroundColor: cColorsWhite,
+        title: Text(
+          "${widget.school.nameSchool}",
+          style: kAppBarText,
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: cColorsBlue,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        child: ListView(
+          children: [
+            SchoolMap(),
+          ], 
+        ),
+      ),
     );
   }
 }
