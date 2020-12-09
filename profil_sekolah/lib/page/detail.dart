32c08aed04bcc07a8beb01a546fa402b230849fc
Begal <50991950+vicco7s@users.noreply.github.com>
2020-12-09 16:47:39 +0800

    Update detail.dart
    update 2020 decembers 9

--- profil_sekolah/lib/page/detail.dart
+++ profil_sekolah/lib/page/detail.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:profil_sekolah/models/modelFirebase.dart';
 import 'package:profil_sekolah/util/const.dart';
 
@@ -13,30 +16,77 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        centerTitle: true,
-        backgroundColor: cColorsWhite,
-        title: Text(
-          "${widget.school.nameSchool}",
-          style: kAppBarText,
-        ),
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back_ios,
-            color: cColorsBlue,
+      // appBar: AppBar(
+      //   automaticallyImplyLeading: true,
+      //   elevation: 0.0,
+      //   centerTitle: true,
+      //   backgroundColor: cColorsWhite,
+      //   title: Text(
+      //     "${widget.school.nameSchool}",
+      //     style: kAppBarText,
+      //   ),
+      //   leading: IconButton(
+      //     icon: Icon(
+      //       Icons.arrow_back_ios,
+      //       color: cColorsBlue,
+      //     ),
+      //     onPressed: () {
+      //       Navigator.of(context).pop(context);
+      //     },
+      //   ),
+      // ),
+      body: Column(
+        children: [
+          MapSchool(
+            location: LatLng(
+              double.parse(widget.school.lat),
+              double.parse(widget.school.long),
+            ),
+            school: widget.school,
           ),
-          onPressed: () {
-            Navigator.of(context).pop(context);
-          },
-        ),
+        ],
       ),
-      body: Container(
-        child: ListView(
-          children: [
-            SchoolMap(),
-          ], 
-        ),
+    );
+  }
+}
+
+class MapSchool extends StatelessWidget {
+  final LatLng location;
+  final SchoolModel school;
+
+  MapSchool({this.location, this.school});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: EdgeInsets.all(5.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: GoogleMap(
+                mapType: MapType.hybrid,
+                initialCameraPosition: CameraPosition(
+                  target: location,
+                  zoom: 20,
+                ),
+                markers: <Marker>[
+                  Marker(
+                      markerId: MarkerId(school.nameSchool),
+                      position: location,
+                      infoWindow: InfoWindow(title: school.nameSchool))
+                ].toSet(),
+                gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[
+                  Factory<OneSequenceGestureRecognizer>(
+                      () => ScaleGestureRecognizer()),
+                ].toSet(),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
